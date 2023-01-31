a7f198990c660afca87d2444d4b0cb1a2e18e018
Ionut <ionut_c1999@yahoo.com>
2023-01-31 18:46:20 +0200

    Cleared comment

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(): Response
-    { //asdad
+    {
         return $this->render('home/index.html.twig');
     }
 }
