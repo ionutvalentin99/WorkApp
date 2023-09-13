4248fcc7df30469c110809c3767b173bb2ea25d8
Ionut <ionut_c1999@yahoo.com>
2023-09-13 16:22:08 +0300

    13.09.2023 update

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +12,11 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        /** @var User $user */
+        $user = $this->getUser()->getFirstName();
+
+        return $this->render('home/index.html.twig', [
+        'user' => $user
+        ]);
     }
 }
