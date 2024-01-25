06018fed9223144f869044d81265f930fb5095c8
ionutvalentin99 <113502637+ionutvalentin99@users.noreply.github.com>
2024-01-25 22:55:31 +0200

    test

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -12,6 +12,7 @@ class UserController extends AbstractController
     #[Route('/user', name: 'app_user')]
     public function index(UserRepository $users): Response
     {
+        // comment test
         return $this->render('user/index.html.twig', [
             'users' => $users->findAll(),
         ]);
