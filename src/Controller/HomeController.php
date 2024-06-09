71f34ecd45fb311bc7dc59963c56480a2d2cff0b
Ionut <ionut_c1999@yahoo.com>
2024-06-10 01:48:43 +0300

    created service for email verify

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,20 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Security\EmailVerifier;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use App\Service\EmailVerificationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
-use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    public function __construct(private readonly EmailVerifier $emailVerifier)
-    {
-    }
-
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
@@ -34,19 +28,11 @@ class HomeController extends AbstractController
      * @throws TransportExceptionInterface
      */
     #[Route('/email/sendVerification', name: 'app_send_email_verification')]
-    public function secondVerifyEmail(): Response
+    public function secondVerifyEmail(EmailVerificationService $emailVerification): Response
     {
         /** @var User $user */
         $user = $this->getUser();
-
-        $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-            (new TemplatedEmail())
-                ->from(new Address('test@pontaje.ro', 'Test'))
-                ->to($user->getEmail())
-                ->subject('Please Confirm your Email')
-                ->htmlTemplate('registration/confirmation_email.html.twig')
-        );
-
+        $emailVerification->sendEmail($user);
         $this->addFlash('success', 'An email confirmation has been sent!');
 
         return $this->redirectToRoute('app_home');
