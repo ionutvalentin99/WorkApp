b3b6175c9518ed42fe91976b53f5be4a96a5881a
Ionut <ionut_c1999@yahoo.com>
2024-07-24 02:46:45 +0300

    added companies

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -2,17 +2,46 @@
 
 namespace App\Controller;
 
+use App\Entity\Company;
+use App\Entity\User;
+use App\Form\CompanyType;
+use App\Repository\CompanyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/user/company')]
 class CompanyController extends AbstractController
 {
-    #[Route('/company', name: 'app_company')]
+    #[Route('/', name: 'app_company')]
     public function index(): Response
     {
-        return $this->render('company/index.html.twig', [
-            'controller_name' => 'CompanyController',
+        return $this->render('company/index.html.twig');
+    }
+
+    #[Route('/new', name: 'app_company_new')]
+    public function new(Request $request, CompanyRepository $companyRepository): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        if (null !== $user->getCompany()) {
+            return $this->redirectToRoute('app_home');
+        }
+        $company = new Company();
+        $company->setOwner($user);
+        $form = $this->createForm(CompanyType::class, $company);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setCompany($company);
+            $companyRepository->save($company, true);
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('company/new.html.twig', [
+            'form' => $form
         ]);
     }
 }
