c19e09a080e5f3222b345738b913068a2d80173e
Ionut <ionut_c1999@yahoo.com>
2024-07-30 02:05:21 +0300

    fixes + added payments

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -2,57 +2,61 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\CompanyRepository;
+use App\Service\StripeService;
+use Doctrine\ORM\EntityManagerInterface;
 use Stripe\Exception\ApiErrorException;
-use Stripe\StripeClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 #[Route('/payment')]
 class PaymentController extends AbstractController
 {
-    #[Route('', name: 'app_stripe_payment')]
-    public function index(): Response
-    {
-        return $this->render('payment/index.html.twig');
-    }
-
     /**
      * @throws ApiErrorException
      */
     #[Route('/checkout', name: 'app_stripe_checkout')]
-    public function checkout($stripeSK): Response
+    public function checkout(StripeService $stripeService): Response
     {
-        $stripe = new StripeClient($stripeSK);
-        $checkout_session = $stripe->checkout->sessions->create([
-            'line_items' => [[
-                'price_data' => [
-                    'currency' => 'ron',
-                    'product_data' => [
-                        'name' => 'Company',
-                    ],
-                    'unit_amount' => 2500,
-                ],
-                'quantity' => 1,
-            ]],
-            'mode' => 'payment',
-            'success_url' => $this->generateUrl('app_stripe_success', [], UrlGeneratorInterface::ABSOLUTE_URL),
-            'cancel_url' => $this->generateUrl('app_stripe_failed', [], UrlGeneratorInterface::ABSOLUTE_URL),
-        ]);
+        $checkout_session = $stripeService->checkout();
 
         return $this->redirect($checkout_session->url, 303);
     }
 
     #[Route('/success', name: 'app_stripe_success')]
-    public function success(): Response
+    public function success(EntityManagerInterface $manager, Request $request): Response
     {
-        return $this->render('payment/success.html.twig');
+        //TODO: on future implement stripe webhook
+        /** @var User $user */
+        $user = $this->getUser();
+        $company = $user->getCompany();
+        $sessionId = $request->query->get('session_id');
+        $companyId = $company?->getId();
+        if ($sessionId && ($request->query->get('company_id') == $companyId)) {
+            $company->setIsPaid(true);
+            $manager->flush();
+
+            return $this->render('payment/success.html.twig');
+        }
+
+        return $this->redirectToRoute('app_stripe_failed');
     }
 
     #[Route('/failed', name: 'app_stripe_failed')]
-    public function failed(): Response
+    public function failed(CompanyRepository $repository): Response
     {
+        /** @var User $user */
+        $user = $this->getUser();
+        $company = $user->getCompany();
+        if (!$company || $company->isPaid()) {
+            return $this->redirectToRoute('app_home');
+        }
+        $user->setCompany(null);
+        $repository->remove($company, true);
+
         return $this->render('payment/failed.html.twig');
     }
 }
